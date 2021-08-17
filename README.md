17da22649dcac7302a7c5b3fa8214b56a612483d
Glenn Song <glenn@nixhydragames.com>
2021-08-16 23:33:13 -0700

    Initiali checkin for Kanji and vocab builder.

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # GoogleSheetsKanjiHelper
-Google spreadsheet  tool to help pull kanji and related vocabulary words from text.
+Google spreadsheet tool to help pull Kanji and related vocabulary words from text.
+
+# Overview 
+
+So, I've been reading a Japanese novel to teach myself how to read Japanese. It's either a brilliant idea or a dumb one. I was manually looking up kanji and vocabulary so I thought a tool could help me do that. Turns out I couldn't find anything and it seemed like a fun programming challenge. I took most of the afternoon and evening to put it together so it's probably janky and buggy.
+
+I developed these Google spreadsheet tools to help me learn Kanji and related vocabulary while reading a book. I was doing the work by hand copying down kanji characters I didn't know and looking up the definition and readings, but that was a slow process. These tools are meant to help me pull that information together faster when used in combination with an OCR scanner (such as the Google Translate app).
+
+I'm not 100% sure if it'll help me or just be a crutch, but hey, it's a tool and maybe you might think it's useful for your learning needs.
+
+# How It Works
+
+First you need some text. I tested using Google Translate's app and took a photo of a page from the book I'm reading. From there I allowed Google Translate to scan it and detect the characters via it's OCR system. It does an okay job. There are some characters that come out wrong, but I can deal with that by manually looking them up. Once you have the text add it to a google spreadsheet.
+
+Add these tools to your google spreadsheet document and give it permission to fetch data from the third party API [kanjiapi](https://kanjiapi.dev/). You're probably wondering how you can do that, or why I didn't deploy this as a script add-on to Google Spreadsheet. I didn't deploy it because I didn't feel like it. Maybe I'll do it later. Goto Tools->Script Editor and copy and paste the code into there. Then set the permissions and it should run for your spreadsheet.
+
+# Function Calls
+
+To get a list of unique kanji call:
+
+```=buildKanjiTable(A1)```
+
+'A1' is the cell where you stashed your text, but you can stash it where ever, just pass it the right cell. This function will go through the text and pull out all of the unique kanji characters and build a table of the characters, konyomi reading, onyomi reading, definition, grade school level, and number of times it saw it in the text. 
+
+To build a partial list of vocabulary call: 
+
+```=buildJapaneseWordTable(A1)```
+
+It uses the same text and runs through building words and tries to find the Hiragana and English definition. It won't find all of the vocabulary, because I don't know how to do that yet. For instance verbs aren't caught since the algorithm does exact matching with the words found in via the API. Anything that's a mix of Kanji and Kana are ignored as well, it's purely for just Kanji words. So, that's something... 
+
+
+If you want to run both the Kanji and Vocab builder use this: 
+
+```=BuildJapaneseWorksheet(A1) ```
+
+I don't call this and who knows if it works. I don't call it because fetching data from the API and processing it causes it to time out. Turns out Google has a limit on how long a process can execute.
+
+# Usage with Anki
+
+One additional thing you can do with a spreadsheet of Kanji and vocabulary data is to import it into Anki and turn it into flashcards. It's not to difficult to do that part either.
+
+Here's how: 
+
+1. I got the [Spreadsheet Import Plus add-on for Anki](https://ankiweb.net/shared/info/716643677). Follow the instructions there to install it into Anki.
+2. With this add-on you have to build your Google Spreadsheet in a specific format. The required layout info is in the link above, but it wasn't that hard to do. I'm a dummy about instructions and I got it the first try. If you're using my functions above you'll probably want to copy/paste the resultant Kanji table to a new google spreadsheet document, format it properly for the add-on, and export that as a Microsoft Excel spreadsheet. I also added the "tags" field so I could take advantage of Anki's tagging system.
+3. Before you import into Anki, make sure you have your own Anki card type setup. I cloned a basic one and added the necessary fields for the kanji, onyomi, konyomi, etc. 
+4. Make a new empty deck in Anki
+5. Import is as easy as file->open in Anki and selecting your spreadsheet.
+6. It'll ask you where to map your data. Make sure your new card type and empty deck are selected. When you're good let Anki do it's biz.
+7. You should have a deck full of kanji to study in the end. Or vocabulary. Or both. The sky's the limit.
+
+# Support
+
+As long as I'm finding the tools useful I'll fix bugs and issues with it. If you want to discuss new features, I'm game to listen, but I built these tools for my needs, so any new implementation will probably be low priority.
+
---
+++ src/KanjiHelper.js
@@ -0,0 +1,415 @@
+/**
+ To get product of three numbers
+ 
+ @param num1 Enter the first number
+ @param num2 Enter the second number
+ @param num3 Enter the third number
+ @customfunction
+ */
+function testfunc(num1, num2, num3) {
+  return num1 * num2 * num3;  
+}
+
+const kMinWordLen = 2;
+
+/**
+ * getKanji uses a third party API to pull in Kanji data. 
+ * 
+ * @param kanjiChar - the Japanese character to get data for.
+ * @customfunction
+ */
+function getKanji(kanjiChar) 
+{
+  //format as a two-dimensional array for google spreadsheet.
+  return [getKanjiArray(kanjiChar)];
+}
+
+function getKanjiArray(kanjiChar) 
+{
+  var url = "https://kanjiapi.dev/v1/kanji/" + kanjiChar;
+  var response = UrlFetchApp.fetch(url);
+  var json = response.getContentText();
+  var data = JSON.parse(json);
+  //Logger.log(data);
+
+  var kun_reading = "";
+  if(data["kun_readings"].length > 0)
+  {
+    kun_reading = data["kun_readings"][0];
+  }
+
+  var on_reading = "";
+  if(data["on_readings"].length > 0)
+  {
+    on_reading = data["on_readings"][0];
+  }
+
+  return [kun_reading, on_reading, data["grade"], data["meanings"].join(', ')];
+}
+
+/** 
+ * Given a character string in kanji, find a word by feeding the first character into the api call.
+ * Note: This won't find verbs most likely...
+ * 
+ * @param getJapaneseWord - the Japanese character to get data for.
+ * @customfunction
+ */
+function getJapaneseWord(kanjiStr) 
+{
+  var data = getJapaneseWordData(kanjiStr);
+  if(data !== null) 
+  {
+    return [[kanjiStr, data.pronounced, data.meanings]];
+  }
+  else 
+  {
+    return [[kanjiStr, "none", "no definition"]];
+  }
+}
+
+//This version of the function can return more than one word potentially.
+function getJapaneseWords(kanjiSearchChar, permutationList) 
+{
+  var url = "https://kanjiapi.dev/v1/words/" + kanjiSearchChar;
+  var response = UrlFetchApp.fetch(url);
+  var json = response.getContentText();
+  var data = JSON.parse(json);
+
+  var results = [];
+
+  //run through the list of results and find the one that matches the kanji string.
+  for(var setIndex = 0; setIndex < data.length; ++setIndex)
+  {
+    var variantSet = data[setIndex];
+    for(var index = 0; index < variantSet.variants.length; ++index)
+    {
+      var variantInfo = variantSet.variants[index];
+
+      //search against all of the permutations that start with the character.
+      for(var perIndex = 0; perIndex < permutationList.length; ++perIndex) 
+      {
+        if(variantInfo.written == permutationList[perIndex]) 
+        {
+          //get the meanings
+          var meanings = null;
+          if(variantSet.meanings.length > 0) 
+          {
+            meanings = variantSet.meanings[0].glosses.join(', ');
+          }
+
+          results.push({
+            "word" : variantInfo.written,
+            "pronounced" : variantInfo.pronounced,
+            "meanings" : meanings
+          });
+        }
+      }
+    }
+  }
+
+  return results;
+}
+
+function getJapaneseWordData(kanjiStr) 
+{
+  if(kanjiStr.length <= 0)
+  {
+    return null;
+  }
+
+  var firstKanji = kanjiStr[0]
+
+  var url = "https://kanjiapi.dev/v1/words/" + firstKanji;
+  var response = UrlFetchApp.fetch(url);
+  var json = response.getContentText();
+  var data = JSON.parse(json);
+
+  //run through the list of results and find the one that matches the kanji string.
+  for(var setIndex = 0; setIndex < data.length; ++setIndex)
+  {
+    var variantSet = data[setIndex];
+    for(var index = 0; index < variantSet.variants.length; ++index)
+    {
+      var variantInfo = variantSet.variants[index];
+      if(variantInfo.written == kanjiStr) 
+      {
+        //get the meanings
+        var meanings = null;
+        if(variantSet.meanings.length > 0) 
+        {
+          meanings = variantSet.meanings[0].glosses.join(', ');
+        }
+
+        return {
+          "pronounced" : variantInfo.pronounced,
+          "meanings" : meanings
+        }
+      }
+    }
+  }
+
+  return null;  
+}
+
+
+//we have a blob of kanji... it could be a word or multiple words, but how do we know?
+//we'll have to look at every permutation since we're not to smart. Carve the kanji up
+//into substrings.
+//
+//Because the API we're using searches via the first character and returns all words
+//starting with that character, we want a dictionary of start characters to permutations
+//so we can pass in the search permutations.
+function buildPermutationDic(buffer) 
+{
+  var permutationDic = {};
+
+  //Logger.log("buildPermutationDic start, buffer is " + buffer);
+
+  if(buffer !== null && buffer.length < kMinWordLen) 
+  {
+    return null;
+  }
+
+  var start = 0;
+  while(start < buffer.length-1) 
+  {
+    permutationDic[buffer[start]] = [];
+
+    for(var minStart = 2; (start + minStart) <= buffer.length; ++minStart)
+    {
+      permutationDic[buffer[start]].push(buffer.substring(start, (start + minStart)));
+    }
+    start++;
+  }        
+
+  //Logger.log("dic is " + JSON.stringify(permutationDic));
+  return permutationDic;
+  
+  //start = 0
+  //minStart = 2
+
+  //ABCDE
+  /* 
+    What the output should be.
+    AB
+    ABC
+    ABCD
+    ABCDE
+    BC
+    BCD
+    BCDE
+    CD
+    CDE
+    DE
+  */
+}
+
+/**
+ * Look at groupings of kanji and build a list of strings.
+ */
+function buildJapaneseWordDic(textStr) 
+{
+  var wordDic = {};
+  var buffer = "";
+
+  for(var i=0; i<textStr.length; ++i) 
+  {
+    var unicodeVal = textStr[i];
+
+    //test if it's a kanji character
+    if(isKanji(textStr.charCodeAt(i)))
+    {
+      buffer += unicodeVal;
+    }
+    else if(buffer.length >= kMinWordLen)
+    {
+      //handle different word permutations
+      var permutationDic = buildPermutationDic(buffer);
+      //Logger.log("permutationDic is " + JSON.stringify(permutationDic));
+      if(permutationDic !== null) 
+      {
+        for(var key in permutationDic) 
+        {
+          var results = getJapaneseWords(key, permutationDic[key]);
+          if(results != null && results.length > 0)
+          {
+            for(var resultIndex = 0; resultIndex < results.length; ++resultIndex) 
+            {
+              var result = results[resultIndex];
+              if(wordDic[result.word])
+              {
+                wordDic[result.word].count += 1;
+              }
+              else
+              {
+                wordDic[result.word] = {
+                  "wordInfo" : result, 
+                  "count" : 1
+                }
+              }
+            }
+          }
+        }        
+      }
+
+      /*
+      //copy the word over
+      var word = (' ' + buffer).slice(1);
+      Logger.log("buffer is " + buffer + ", word is " + word);
+      if(wordDic[word])
+      {
+        wordDic[word].count += 1;
+      }
+      else
+      {
+        wordDic[word] = {
+          "wordInfo" : getJapaneseWordData(word),
+          "count" : 1
+        }
+      }*/
+
+      //clear the buffer ready for the next word.
+      buffer = "";
+    }
+    else 
+    {
+      buffer = "";
+    }
+  }
+
+  return wordDic;
+}
+
+//helper function to test if the letter is a valid kanji unicode
+function isKanji(val) 
+{
+  return (val >= 0x4E00 && val <= 0x9FAF);
+}
+
+/**
+ * Given a text string returns an array of all the unique kanji in a text string.
+ * @param textStr Japanese text to process and build a kanji table from.
+ * @customfunction
+ */
+function buildKanjiTable(textStr) 
+{
+  var output = [];
+  output.push(["Kanji Table"]);
+  output.push(["kanji", "konyomi", "onyomi", "grade", "definition", "count"]);
+
+  var kanjiDic = buildKanjiTableData(textStr);  
+  for(var key in kanjiDic) 
+  {
+    var kanjiInfo = kanjiDic[key].kanjiInfo;
+  
+    //add the kanji character to the front
+    kanjiInfo.unshift(key);
+    kanjiInfo.push(kanjiDic[key].count);
+    output.push(kanjiInfo);
+  }
+
+  return output;
+}
+
+/**
+ * Build the dictionary of unique kanji in the text
+ */
+function buildKanjiTableData(textStr) 
+{
+  var kanjiDic = {};
+
+  // Kanji unicode: 	\u4E00-\u9FAF
+  // Build unique set of kanji characters
+  // character order is how it was found in the text.
+  for(var i=0; i<textStr.length; ++i) 
+  {
+    var unicodeVal = textStr[i];
+
+    //test if it's a kanji character
+    if(isKanji(textStr.charCodeAt(i)))
+    {
+      //if we have this character, ignore it.
+      if(kanjiDic[unicodeVal]) 
+      {
+        kanjiDic[unicodeVal].count += 1;
+      }
+      else 
+      {
+        kanjiDic[unicodeVal] = {
+          kanjiInfo: getKanjiArray(unicodeVal),
+          count: 1
+        };
+      }
+    }
+  }
+
+  return kanjiDic;
+}
+
+/**
+ * Given a text string returns an array of all the unique kanji based vocab words
+ * @param textStr Japanese text to process and build a kanji table from.
+ * @customfunction
+ */
+function buildJapaneseWordTable(textStr) 
+{
+  var output = [];
+  output.push(["Vocabulary List"]);
+
+  //get words made out of kanji and print them out.
+  var wordDic = buildJapaneseWordDic(textStr)
+  for(var key in wordDic)
+  {
+    var wordInfo = wordDic[key].wordInfo;
+
+    if(wordInfo !== null) 
+    {
+      output.push([key, wordInfo.pronounced, "", "", wordInfo.meanings, wordDic[key].count]);
+    }
+    else 
+    {
+      output.push([key, "none", "", "", "no definition", 0]);
+    }
+  }
+
+  return output;
+}
+
+/**
+ * Build kanji and word tables -- but this can exceed the max allowed runtime for a goodgle spreadsheet.
+ */
+function BuildJapaneseWorksheet(textStr) 
+{
+  var output = [];
+
+  var kanjiDic = buildKanjiTableData(textStr);  
+  for(var key in kanjiDic) 
+  {
+    var kanjiInfo = kanjiDic[key].kanjiInfo;
+    //add the kanji character to the front
+    kanjiInfo.unshift(key);
+    kanjiInfo.push(kanjiDic[key].count);
+    output.push(kanjiInfo);
+  }
+
+  output.push([]);
+  output.push(["Vocabulary List"]);
+
+  //get words made out of kanji and print them out.
+  var wordDic = buildJapaneseWordDic(textStr)
+  for(var key in wordDic)
+  {
+    var wordInfo = wordDic[key].wordInfo;
+
+    if(wordInfo !== null) 
+    {
+      output.push([key, wordInfo.pronounced, "", "", wordInfo.meanings, wordDic[key].count]);
+    }
+    else 
+    {
+      output.push([key, "none", "", "", "no definition", 0]);
+    }
+  }
+
+  return output;
+}
